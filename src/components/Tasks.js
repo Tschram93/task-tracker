34c89d2e1a78769ff51dba90b2538e5ba664cfe1
TS93 <tschram93@yahoo.com>
2021-12-16 12:13:24 -0500

    tasks function created. Example used

---
+++ src/components/Tasks.js
@@ -0,0 +1,31 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Schedule vaccination appointment',
+        day: 'Dec. 16 12:30PM',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Get ready to go to appointment',
+        day: 'Dec. 16 1:00PM',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Arrive at clinic',
+        day: 'Dec. 16 2:00PM',
+        reminder: true,
+    }
+];
+
+
+const Tasks = () => {
+    return (
+        <>
+            {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+        </>
+    )
+}
+
+export default Tasks
