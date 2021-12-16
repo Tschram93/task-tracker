6a7091e8d0aa6ab8a67e71438c36272446f8d570
TS93 <tschram93@yahoo.com>
2021-12-16 12:21:18 -0500

    destructured tasks prop and moved useState to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import Header from './components/Header';
 import Tasks from "./components/Tasks";
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Schedule vaccination appointment',
+        day: 'Dec. 16 12:30PM',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Get ready to go to appointment',
+        day: 'Dec. 16 1:00PM',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Arrive at clinic',
+        day: 'Dec. 16 2:00PM',
+        reminder: true,
+    }
+])
+
 	return (
 		<div className="container">
 			<Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
 		</div>
 	);
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,5 @@
-import { useState } from "react";
 
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Schedule vaccination appointment',
-            day: 'Dec. 16 12:30PM',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Get ready to go to appointment',
-            day: 'Dec. 16 1:00PM',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Arrive at clinic',
-            day: 'Dec. 16 2:00PM',
-            reminder: true,
-        }
-    ])
+const Tasks = ({tasks}) => {
     return (
         <>
             {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
