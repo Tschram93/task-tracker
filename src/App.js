ac4e1cbea9069a917d1036967e0e4c6c95fff890
TS93 <tschram93@yahoo.com>
2021-12-16 12:48:38 -0500

    displays message if no current tasks in list

--- src/App.js
+++ src/App.js
@@ -27,13 +27,13 @@ function App() {
 
 // Delete Task
 const deleteTask = (id) => {
-  console.log('Delete:', id);
+  setTasks(tasks.filter((task) => task.id !==id));
 }
 
 	return (
 		<div className="container">
 			<Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ('No Current Tasks')}
 		</div>
 	);
 }
