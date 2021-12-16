d3e8555404ee7ef0abaff9d4e18c5f83a3704f7b
TS93 <tschram93@yahoo.com>
2021-12-16 12:17:11 -0500

    put example tasks into a tasks useState hook

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,26 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Schedule vaccination appointment',
-        day: 'Dec. 16 12:30PM',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Get ready to go to appointment',
-        day: 'Dec. 16 1:00PM',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Arrive at clinic',
-        day: 'Dec. 16 2:00PM',
-        reminder: true,
-    }
-];
-
+import { useState } from "react";
 
 const Tasks = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Schedule vaccination appointment',
+            day: 'Dec. 16 12:30PM',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Get ready to go to appointment',
+            day: 'Dec. 16 1:00PM',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Arrive at clinic',
+            day: 'Dec. 16 2:00PM',
+            reminder: true,
+        }
+    ])
     return (
         <>
             {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
